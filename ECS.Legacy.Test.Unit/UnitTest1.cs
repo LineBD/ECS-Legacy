c4fa086e6d0447c202d6f8d6b701c2d0b8d5deae
Rikke <201607161@post.au.dk>
2018-02-19 12:11:01 +0100

    test ændret

--- ECS.Legacy.Test.Unit/UnitTest1.cs
+++ ECS.Legacy.Test.Unit/UnitTest1.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NUnit.Framework;
+
 
 namespace ECS.Legacy.Test.Unit
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ECS _uut;
+        private IHeater _heater;
+        private ITempSensor _tempSensor;
+
+        [SetUp]
+        public void Setup()
         {
+            _heater = Substitute.For<IHeater>();
+            _tempSensor = Substitute.For<ITempSensor>();
+
+            _uut = new ECS(25, _tempSensor,_heater);
         }
+
+        [Test]
+        public void RunSelfTest_TempSensorFails_SelfTestFails()
+        {
+            _tempSensor.RunSelfTest().Returns(false);
+            _heater.RunSelfTest().Returns(true);
+            NUnit.Framework.Assert.IsFalse(_uut.RunSelfTest());
+        }
+
+        [Test]
+        public void Regulate_TempIrrelevant_TempSensorQueriedForTemp()
+        {
+            _uut.Regulate();
+            _tempSensor.Received().GetTemp();
+        }
+
+
     }
 }
